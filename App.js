3bd8ff366c32cb7ddc302f9c43ca34728d1f224b
la2ure <2avlaure@gmail.com>
2024-04-15 19:25:17 +0200

    Creation of chats screen and integration of data and images folder.

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import ChatListItem from "./src/components/ChatListItem";
-
-const chat = {
-  id: "1",
-  user: {
-    image:
-      "https://images.unsplash.com/photo-1624313603844-dda78ecf39b8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fGNoYXJhY3RlcnxlbnwwfHwwfHx8MA%3D%3D",
-    name: "Lukas",
-  },
-  lastMessage: {
-    text: "Hi, are you free next saturday ? I want to visit the new science exposition !🍄",
-    createdAt: "8:00",
-  },
-};
+import ChatsScreen from "./src/screens/chats/ChatsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ChatListItem chat={chat} />
+      <ChatsScreen />
 
       <StatusBar style="auto" />
     </View>
@@ -29,7 +16,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+
+    paddingVertical: 50,
   },
 });
