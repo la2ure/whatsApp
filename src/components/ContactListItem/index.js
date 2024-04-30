5539a0e2ef5477cdb7171ec84f48de5d1b19920a
la2ure <2avlaure@gmail.com>
2024-04-30 16:40:49 +0200

    Create data models; pull models &graphql folders locally, queries with graphql api users seeds & last message on contact screen.

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -45,6 +45,9 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     marginRight: 10,
   },
+  content: {
+    flex: 1,
+  },
   name: {
     fontWeight: "bold",
   },
--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -1,12 +1,26 @@
+// To store the queries, run queries when component mounts
+import { useState, useEffect } from "react";
+
 import { FlatList } from "react-native";
-import chats from "../../assets/data/chats.json";
 import ContactListItem from "../components/ContactListItem";
 
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsers } from "../graphql/queries";
+
 const ContactsScreen = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    API.graphql(graphqlOperation(listUsers)).then((result) => {
+      console.log(result);
+      setUsers(result.data?.listUsers?.items);
+    });
+  }, []);
+
   return (
     <FlatList
-      data={chats}
-      renderItem={({ item }) => <ContactListItem user={item.user} />}
+      data={users}
+      renderItem={({ item }) => <ContactListItem user={item} />}
       style={{ backgroundColor: "white" }}
     />
   );
